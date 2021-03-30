0ec6a64f17a2402c8771631d0f59bc5c11d35c99
ravargs <rajuvarghese@gmail.com>
2021-03-30 15:09:33 +0530

    Update ApplicationTest.java

--- src/test/java/com/example/helloworld/ApplicationTest.java
+++ src/test/java/com/example/helloworld/ApplicationTest.java
@@ -8,8 +8,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
-
+// FIXME Nobody knows why this is here
 	@Test
 	public void contextLoads() {
 	}
 }
+// TODO: Ideally this would be working
+
+// TODO: Not sure why this works, investigate
+
+// @todo: This worked last Monday 
+
+
